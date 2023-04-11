31e1998c411dbbf8feee84c4a260f8b4bac9f230
PavliukD <albatrozzz1@gmail.com>
2023-04-11 12:02:24 +0300

    build

---
+++ build/789225261fc5417df8e8.js
@@ -0,0 +1,1 @@
+import style from"./main.scss";import html from"./index.html";import{CloseNotificaton}from"./js/closeNotification";import{ChaneLanguage}from"./js/language";import{DropCall}from"./js/drop-call";import{Cookies}from"./js/cookies";import{Hero}from"./js/hero";CloseNotificaton(),ChaneLanguage(),DropCall(),Cookies(),Hero();
