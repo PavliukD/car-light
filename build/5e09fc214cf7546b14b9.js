59cde18b764dd73983452b338cb9f35095d2023d
PavliukD <albatrozzz1@gmail.com>
2023-04-11 18:07:07 +0300

    add bade styles for services section (without bg)

--- build/5e09fc214cf7546b14b9.js
+++
@@ -1,1 +0,0 @@
-import style from"./main.scss";import html from"./index.html";import{CloseNotificaton}from"./js/closeNotification";import{ChaneLanguage}from"./js/language";import{DropCall}from"./js/drop-call";CloseNotificaton(),ChaneLanguage(),DropCall();
